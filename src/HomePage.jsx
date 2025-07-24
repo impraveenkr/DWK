21f9b352d63bfb827a539e7772f4caa093ff5210
Praveen Kumar <praveen@adprex.com>
2025-07-24 23:00:56 +0530

    Initial commit of Digital Web King homepage

---
+++ src/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <main className="bg-white text-black font-inter text-center py-40 px-8">
+      <h1 className="text-4xl font-bold">Digital Web King Homepage</h1>
+      <p className="text-gray-500 mt-4">Preview coming soon...</p>
+    </main>
+  );
+}
